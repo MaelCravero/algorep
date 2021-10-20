96e786190fffa56292ac9c26c2b4fd6d717cce31
Aleksandar Radojicic <aleksandar.radojicic@epita.fr>
2021-10-20 17:57:42 +0200

    add server constructor and update method

--- src/server.cc
+++ src/server.cc
@@ -1,13 +1,46 @@
 #include "server.hh"
 
+#include <chrono>
+#include <random>
+
 void server(int rank, int size)
 {
     Server server(rank, size);
 
-    // TODO
+    while (true)
+    {
+        server.update();
+    }
 }
 
 Server::Server(int rank, int size)
-    : rank_(rank)
+    : status_(Status::FOLLOWER)
+    , rank_(rank)
     , size_(size)
-{}
+    , leader_rank_(-1)
+    , next_index_(0)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0.1, 0.5);
+
+    election_timeout_ = dis(gen);
+    election_time_remaining_ = election_timeout_;
+
+    previous_time_ = std::chrono::system_clock::now().time_since_epoch();
+
+    term_ = 0;
+}
+
+void Server::update()
+{
+    if (status_ != Status::LEADER)
+    {
+        // TODO update time
+
+        // auto now = std::chrono::system_clock::now().time_since_epoch();
+        // election_time_remaining_ -= fp_ms;
+
+        // TODO: election ?
+    }
+}
--- src/server.hh
+++ src/server.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <chrono>
 #include <mpi.h>
 #include <vector>
 
@@ -27,6 +28,10 @@ public:
     AppendEntriesStatus append_entries(std::string log);
 
     void request_vote();
+
+    // Update server, if timeout is reached then start an election
+    void update();
+
     int get_leader_rank();
 
 private:
@@ -39,6 +44,9 @@ private:
     std::vector<size_t> next_index_;
 
     double election_timeout_;
+    double election_time_remaining_;
+
+    std::chrono::duration<double> previous_time_;
     size_t term_;
 };
 
