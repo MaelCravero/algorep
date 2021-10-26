67f72f59ebb431947bc25618246352bcec7b8aef
Aleksandar Radojicic <aleksandar.radojicic@epita.fr>
2021-10-26 18:05:26 +0200

    repl: add start command

--- src/repl.hh
+++ src/repl.hh
@@ -1,29 +1,44 @@
 #pragma once
 
+#include <optional>
+#include <string>
+
 #include "common.hh"
 
 class Repl
 {
 public:
-    enum class Order
+    enum class Order : char
     {
-        SPEED,
-        CRASH,
-        START,
-        RECOVERY,
-        STATUS,
+        SPEED = 's',
+        CRASH = 'c',
+        BEGIN = 'b',
+        RECOVERY = 'r',
+        PRINT = 'p',
     };
 
     struct ReplMessage : public Message
     {
         Order order;
-        int argument; // optional argument for some orders
+        int speed_level; // optional argument for some orders
     };
 
-    Repl(int nb_server);
+private:
+    struct Command
+    {
+        Order order;
+        int target;
+        int speed_level;
+    };
 
+public:
+    Repl(int nb_server, int nb_client);
+
+    std::optional<Command> parse_command(std::string line);
+    void execute(Command command);
     void operator()();
 
 private:
     int nb_server_;
+    int nb_client_;
 };
